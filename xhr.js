322ea432d86ed0fcea081a1a8ae199cd69c1309e
Abbas Mashayekh <martianboy2005@gmail.com>
2014-07-01 23:24:02 +0430

    First commit

---
+++ xhr.js
@@ -0,0 +1,97 @@
+define(['underscore'], function(_) {
+"use strict";
+
+function convertData(obj) {
+    return Object.keys(obj).map(function(key) {
+        var data = obj[key];
+        if (typeof data === 'object')
+            data = JSON.stringify(data);
+        return key + "=" + encodeURIComponent(data);
+    }).join("&");
+}
+
+function xhr(options) {
+
+    function xhrPromise(resolve, reject) {
+
+        function onLoad() {
+            function convertResponse(response) {
+                if (!('dataType' in options))
+                    options.dataType = req.getResponseHeader('Content-Type');
+                switch (options.dataType) {
+                    case 'text':
+                    case 'text/plain':
+                        return response;
+                        break;
+                    case 'application/json':
+                    case 'json':
+                    default:
+                        return JSON.parse(response);
+                }
+            }
+            // This is called even on 404 etc
+            // so check the status
+            if (req.status >= 200 && req.status < 300) {
+                // Resolve the promise with the response text
+                resolve(convertResponse(req.response));
+            } else {
+                // Otherwise reject with the status text
+                // which will hopefully be a meaningful error
+                reject({xhr: req, textStatus: req.statusText});
+            }
+        }
+
+        // Handle network errors
+        function onError() {
+            reject({xhr: req, textStatus: req.statusText});
+        };
+
+        // Do the usual XHR stuff
+        var req = new XMLHttpRequest();
+        var method = ('type' in options) ? options.type.toUpperCase() : 'GET';
+
+        req.open(method, options.url);
+
+        var data = options.data;
+        
+        if ('contentType' in options && method !== 'GET') {
+            switch(options.contentType) {
+                case 'application/x-www-form-urlencoded':
+                    if (typeof(data) === 'object')
+                        data = convertData(data);
+                    break;
+                case 'appliation/json':
+                    data = JSON.stringify(data);
+            }
+
+            req.setRequestHeader("Content-type", options.contentType);
+        }
+
+        if ('dataType' in options)
+            req.responseType = options.dataType;
+
+        if ('xhrFields' in options)
+            _.extend(req, options.xhrFields);
+
+        req.addEventListener('load', onLoad);
+        req.addEventListener('error', onError);
+        req.addEventListener('timeout', onError);
+
+        // Make the request
+        if (options.beforesend)
+            options.beforesend(req);
+        req.send(data);
+    }
+
+    // Return a new promise.
+    return new Promise(xhrPromise);
+}
+
+xhr.getJSON = function(url) {
+    return xhr({
+        url: url
+    });
+}
+return xhr;
+
+})
